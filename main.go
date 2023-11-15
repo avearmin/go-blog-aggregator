aecc81e2d6d99f233fb4d014f8be62fca5e458b2
avearmin <arminradoncic1@gmail.com>
2023-11-14 21:32:16 -0500

    Create router and server

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	panic("Put something interesting here.")
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+	port := os.Getenv("PORT")
+
+	mainRouter := chi.NewRouter()
+
+	corsOptions := cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedHeaders: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "OPTIONS", "PUT", "DELETE"},
+	}
+	mainRouter.Use(cors.Handler(corsOptions))
+
+	subRouter := chi.NewRouter()
+	mainRouter.Mount("/v1", subRouter)
+
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: mainRouter,
+	}
+	fmt.Print("Start Success!")
+	server.ListenAndServe()
 }
