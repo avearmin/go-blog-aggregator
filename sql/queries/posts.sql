7869dfcdb66893aebbbc3565e4fe461531924be3
avearmin <arminradoncic1@gmail.com>
2023-12-19 13:15:26 -0500

    Add CreatePost and GetPostByUser queries

---
+++ sql/queries/posts.sql
@@ -0,0 +1,11 @@
+-- name: CreatePost :one
+INSERT INTO posts (id, created_at, updated_at, title, url, description, published_at, feed_id)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+RETURNING *;
+
+-- name: GetPostsByUser :many
+SELECT * FROM posts
+JOIN feeds ON posts.feed_id = feeds.id 
+WHERE feeds.userid = $1
+ORDER BY posts.created_at DESC
+LIMIT $2;
